fa6f00bb0357ad49d3d69963e4e5e77ff87bdae4
sksat <sksat.tec@gmail.com>
2017-11-16 18:05:30 +0900

    [ADD] string test

--- main.cc
+++ main.cc
@@ -5,11 +5,21 @@ int g_var = 0;
 
 int main(int argc, char **argv){
 	using namespace std;
+
+	char str[] = "hello"; // ポインタはダメ
+
 	perpetuate(g_var); // 永続化
+	perpetuate(str);
+
 	persistent::load(); // 永続変数をロード
 
 	cout<<"g_var = "<<g_var<<endl;
+	cout<<"str = "<<str<<endl;
 	g_var++;
+	if(str[0] == ' ')
+		str[0] = 'h';
+	else
+		str[0] = ' ';
 
 	persistent::save(); // 永続変数の保存
 
